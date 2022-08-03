ca5ec934a38f43eea48e41de9e443df50718fab5
naamafrank1 <naama0556772275@gmail.com>
2022-08-03 23:26:52 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3333);
   app.useGlobalPipes(new ValidationPipe());
   console.log(`Application is running on: ${await app.getUrl()}`);
