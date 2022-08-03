f351d72d2b31a29cb703ef1b9dfb77898aed3dce
OraBenesh <orabenesh@gmail.com>
2022-08-03 17:16:01 +0300

    userDto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,18 +1,15 @@
 /* eslint-disable prettier/prettier */
-<<<<<<< HEAD
-export interface UserDto {
-   name: string;
-   age: number;
-   breed: string;
-=======
-import { IsNotEmpty } from 'class-validator';
-import { Hash } from 'crypto';
+// export interface UserDto {
+//    name: string;
+//    age: number;
+//    breed: string;
+// import { Hash } from 'crypto';
 
-export class UserDto {
-  id: Hash
-  @IsNotEmpty()
-  name: string;
-  age: number;
-  breed: string;
->>>>>>> 4d04eca60ab2db062d675c52ea948ed2366c8683
+import { IsNotEmpty } from "class-validator";
+
+export interface UserDto {
+  id: number;
+  firstName: string;
+  lastName: string; 
+  email: string;
 }
